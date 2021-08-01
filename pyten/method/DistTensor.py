2124b1c987c48b84c6328dcd5f9cd4b0d01b8cfc
yjj202 <86861251+yjj202@users.noreply.github.com>
2021-08-01 18:27:37 -0400

    Update DistTensor.py

--- pyten/method/DistTensor.py
+++ pyten/method/DistTensor.py
@@ -239,7 +239,7 @@ class TensorDecompositionALS(object):
         sc.setLogLevel('WARN')
         sc.addPyFile("pyten/tools/utils_tensor.py")
 
-        print "TIME - Setting up the Spark: {} sec.".format(time.time() - start_program)
+        print ("TIME - Setting up the Spark: {} sec.".format(time.time() - start_program))
 
         # start the timer for processing data
         start_process_data = time.time()
@@ -287,7 +287,7 @@ class TensorDecompositionALS(object):
         # calculate the norm of the observed tensor
         normData = np.sqrt(rawData.map(lambda x: x.val * x.val).sum())
 
-        print "INFO - A tensor of the size {}x{}x{} with {} non-zero elements.".format(self.I, self.J, self.K, self.nnz)
+        print ("INFO - A tensor of the size {}x{}x{} with {} non-zero elements.".format(self.I, self.J, self.K, self.nnz))
 
         # initialize the partitioner for each dimension
         iPartitioner = CustomizedPartitioner(self.numIBlocks)
@@ -314,7 +314,7 @@ class TensorDecompositionALS(object):
                               .mapValues(mapValuesFunc)\
                               .persist(self.intermediateRDDStorageLevel)
 
-        print "TIME - Process Raw Tensor Data: {} sec.".format(time.time() - start_process_data)
+        print ("TIME - Process Raw Tensor Data: {} sec.".format(time.time() - start_process_data))
 
         # start the timer for the initialization
         start_init = time.time()
@@ -392,7 +392,7 @@ class TensorDecompositionALS(object):
             if fitchange < self.tol:
                 break
 
-            print "TIME - Update Factor Matrices: {} sec. in {} iterations".format(time.time() - start_inner, iteration)
+            print ("TIME - Update Factor Matrices: {} sec. in {} iterations".format(time.time() - start_inner, iteration))
 
         self.iFactors = iFactors
         self.jFactors = jFactors
@@ -413,8 +413,8 @@ class TensorDecompositionALS(object):
         self.lambdaVals = lambdaVals
         self.ktensor = pyten.tenclass.Ktensor(self.lambdaVals, self.Us)
 
-        print "TIME - Completeing the whole iterations: {} sec.".format(time.time() - start_iter)
-        print "TIME - Total computation time: {} sec.".format(time.time() - start_program)
+        print ("TIME - Completeing the whole iterations: {} sec.".format(time.time() - start_iter))
+        print ("TIME - Total computation time: {} sec.".format(time.time() - start_program))
 
 
 if __name__ == "__main__":
