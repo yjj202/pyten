3672155b02991d7f85eeecf3fbd934da6671367e
yjj202 <86861251+yjj202@users.noreply.github.com>
2021-09-05 23:16:43 -0400

    Update cmtf.py

--- pyten/method/cmtf.py
+++ pyten/method/cmtf.py
@@ -108,8 +108,8 @@ def cmtf(x, y=None, c_m=None, r=2, omega=None, tol=1e-4, maxiter=100, init='rand
         for n in range(N):
             # Calculate Unew = X_(n) * khatrirao(all U except n, 'r').
             temp1 = [n]
-            temp2 = range(n)
-            temp3 = range(n + 1, N)
+            temp2 = list(range(n))
+            temp3 = list(range(n + 1, N))
             temp2.reverse()
             temp3.reverse()
             temp1[len(temp1):len(temp1)] = temp3
